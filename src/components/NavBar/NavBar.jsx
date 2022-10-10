c780571503f4c07acc2d40c4f5d4b0d20fe13037
wabbi-khan <kwahab789@gmail.com>
2022-10-10 10:43:13 +0500

    navbar done

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -45,8 +45,6 @@ const NavBar = () => {
               <Link className='nav-link disabled ms-4'>Disabled</Link>
             </li>
           </ul>
-        </div>
-        <div className='collapse navbar-collapse' id='navbarNav'>
           <ul className='navbar-nav mx-auto text-center nav-icons'>
             <li className='nav-item '>
               <Link className='nav-link' to={"/"}>
@@ -64,22 +62,27 @@ const NavBar = () => {
             </li>
             <li className='nav-item ms-4'>
               <Link className='nav-link' to={"/"}>
-                <svg
-                  xmlns='http://www.w3.org/2000/svg'
-                  width='25'
-                  height='25'
-                  fill='currentColor'
-                  class='bi bi-cart-check'
-                  viewBox='0 0 16 16'
-                >
-                  <path d='M11.354 6.354a.5.5 0 0 0-.708-.708L8 8.293 6.854 7.146a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0l3-3z' />
-                  <path d='M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z' />
-                </svg>
-                <div className='totalProducts'>01</div>
+                <div className='items'>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    width='25'
+                    height='25'
+                    fill='currentColor'
+                    class='bi bi-cart-check'
+                    viewBox='0 0 16 16'
+                  >
+                    <path d='M11.354 6.354a.5.5 0 0 0-.708-.708L8 8.293 6.854 7.146a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0l3-3z' />
+                    <path d='M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z' />
+                  </svg>
+                  <div className='totalProducts'>01</div>
+                </div>
               </Link>
             </li>
           </ul>
         </div>
+        {/* <div className='collapse navbar-collapse' id='navbarNav'> */}
+
+        {/* </div> */}
       </div>
     </nav>
   );
