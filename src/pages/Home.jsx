4ff19c39027578deef695cb4177aef7452030784
wabbi-khan <kwahab789@gmail.com>
2022-10-10 12:29:50 +0500

    hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,29 @@
 import React from "react";
-
+import HeroImg from "../img/hero-img.png";
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className='container-fluid mt-4 hero'>
+      <div className='row'>
+        <div className='col-md-6 text-center mt-4 hero-left'>
+          <div className='hero-left-content'>
+            <h1>Best Place For</h1>
+            <span>watches</span>
+          </div>
+          <div>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Doloremque laudantium dicta sequi sit. Fugiat tempore, dignissimos
+              explicabo
+            </p>
+          </div>
+        </div>
+        <div className='col-md-4 text-end mt-4 hero-right'>
+          <img src={HeroImg} alt='HeroImg' className='mt-4 ' />
+          <div className='hero-circle'></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
