03f22aaa50e1a9b1e770cf47a201b389fe37ca72
wabbi-khan <kwahab789@gmail.com>
2022-10-10 14:38:18 +0500

    structure

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import HeroImg from "../../img/asd.png";
+
+const Hero = () => {
+  return (
+    <div className='container-fluid mt-4 hero'>
+      <div className='row'>
+        <div className='col-md-6  mt-4 hero-left'>
+          <div className='hero-left-content text-center'>
+            <h1>Best Place For</h1>
+            <span>watches</span>
+          </div>
+          <div>
+            <p className=''>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Doloremque laudantium dicta sequi sit. Fugiat tempore, dignissimos
+              explicabo
+            </p>
+            <div className='hero-btn ms-4'>Shop Now</div>
+          </div>
+        </div>
+        <div className='col-md-6 text-end mt-4 hero-right'>
+          {/* <img src={HeroImg} alt='HeroImg' className='mt-4 ' /> */}
+          {/* <div className='hero-circle'></div> */}
+          <img src={HeroImg} alt='hero img' />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "../../Style/style.css";
+const Products = () => {
+  return (
+    <div className='container mt-4'>
+      <div className='row text-center mainRow'>
+        <div className='col-md-6 mainDiv mb-4'>
+          <div className=' d-flex'>
+            <div className='contentDiv  ms-4'>sd</div>
+            <div className='contentDiv  ms-4'>sd</div>
+            <div className='contentDiv  ms-4'>sd</div>
+            <div className='contentDiv  ms-4'>sd</div>
+            <div className='contentDiv  ms-4'>sd</div>
+            <div className='contentDiv  ms-4'>sd</div>
+            <div className='contentDiv  ms-4'>sd</div>
+            <div className='contentDiv  ms-4'>sd</div>
+            <div className='contentDiv  ms-4'>sd</div>
+            <div className='contentDiv  ms-4'>sd</div>
+            <div className='contentDiv  ms-4'>sd</div>
+          </div>
+        </div>
+        <div className='col-md-4 contentDiv ms-4 mb-4'>asdasd</div>
+      </div>
+    </div>
+  );
+};
+
+export default Products;
