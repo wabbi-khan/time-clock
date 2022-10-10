00ac0496927b705a76012f92301db637e90f5e91
wabbi-khan <kwahab789@gmail.com>
2022-10-10 12:40:51 +0500

    hero section almost done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,17 +4,18 @@ const Home = () => {
   return (
     <div className='container-fluid mt-4 hero'>
       <div className='row'>
-        <div className='col-md-6 text-center mt-4 hero-left'>
-          <div className='hero-left-content'>
+        <div className='col-md-6  mt-4 hero-left'>
+          <div className='hero-left-content text-center'>
             <h1>Best Place For</h1>
             <span>watches</span>
           </div>
           <div>
-            <p>
+            <p className=''>
               Lorem ipsum dolor sit, amet consectetur adipisicing elit.
               Doloremque laudantium dicta sequi sit. Fugiat tempore, dignissimos
               explicabo
             </p>
+            <div className='hero-btn ms-4'>Shop Now</div>
           </div>
         </div>
         <div className='col-md-4 text-end mt-4 hero-right'>
