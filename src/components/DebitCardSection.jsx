ebb2da696c965ecdcee66df949f96d2b38b528bb
wabbi-khan <kwahab789@gmail.com>
2022-10-14 20:37:48 +0500

    ok

--- src/components/DebitCardSection.jsx
+++ src/components/DebitCardSection.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 const DebitCardSection = () => {
-  return <div className='debit-bg'>DebitCardSection</div>;
+  return (
+    <div className='debit-bg'>
+      <div className='container debit-content'>
+        <div className='debit-title'>Exclusive offers</div>
+        <div className='debit-para'>
+          Get Upto <span>50%</span> Off On Debit Card
+        </div>
+        <div className='debit-time mt-2'>
+          <div>
+            <p>11</p>
+            <br />
+            <p>Hourse</p>
+          </div>
+          <div>asd</div>
+          <div>asd</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DebitCardSection;
