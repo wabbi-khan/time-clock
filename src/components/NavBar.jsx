ddc03f96fd7665ef2f4460f83f3c3d830f3c208f
wabbi-khan <kwahab789@gmail.com>
2022-10-16 03:18:31 +0500

    ok

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Logo from "./../img/logo.png";
-import { MdOutlineKeyboardBackspace } from "react-icons/md";
-import { RiRefreshFill } from "react-icons/ri";
+// import { MdOutlineKeyboardBackspace } from "react-icons/md";
+// import { RiRefreshFill } from "react-icons/ri";
 import { motion } from "framer-motion";
 import "./../Style/style.css";
 import { useCart } from "react-use-cart";
-import CartItems from "./CartItems";
+// import CartItems from "./CartItems";
 import CartContainer from "./CartContainer";
 const NavBar = () => {
-  const { totalItems, emptyCart } = useCart();
+  const { totalItems } = useCart();
   const [isMenu, setIsMenu] = useState(false);
   return (
     <>
@@ -99,7 +99,9 @@ const NavBar = () => {
                       <path d='M11.354 6.354a.5.5 0 0 0-.708-.708L8 8.293 6.854 7.146a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0l3-3z' />
                       <path d='M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z' />
                     </svg>
-                    <div className='totalProducts'>0{totalItems}</div>
+                    <div className='totalProducts text-center'>
+                      {totalItems}
+                    </div>
                   </div>
                   {isMenu && <CartContainer close={() => setIsMenu(!isMenu)} />}
                 </Link>
