80adbf35a84656c021b0ab15d784639e6e8b5731
wabbi-khan <kwahab789@gmail.com>
2022-10-16 03:07:30 +0500

    ok

--- src/components/MobileHeader.jsx
+++ src/components/MobileHeader.jsx
@@ -1,15 +1,25 @@
 import React from "react";
-
+import { MdHome, MdSearch, MdAddShoppingCart } from "react-icons/md";
+import { AiFillSetting } from "react-icons/ai";
 const MobileHeader = () => {
   return (
     // <div className='container-fluid bg-dark'>
+
     <div className='row mt-4'>
       <div className='col-md-12'>
         <div className='mainMob'>
-          <div className='icon'>asd</div>
-          <div className='icon'>asd</div>
-          <div className='icon'>asd</div>
-          <div className='icon'>asd</div>
+          <div className='icon active'>
+            <MdHome />
+          </div>
+          <div className='icon'>
+            <MdSearch />
+          </div>
+          <div className='icon'>
+            <MdAddShoppingCart />
+          </div>
+          <div className='icon'>
+            <AiFillSetting />
+          </div>
         </div>
       </div>
     </div>
