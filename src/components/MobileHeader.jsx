ec96bac5151d9bdc28d7428353b03e6a76301687
wabbi-khan <kwahab789@gmail.com>
2022-10-16 04:03:49 +0500

    all set

--- src/components/MobileHeader.jsx
+++ src/components/MobileHeader.jsx
@@ -1,29 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { MdHome, MdSearch, MdAddShoppingCart } from "react-icons/md";
 import { AiFillSetting } from "react-icons/ai";
+import CartContainer from "./CartContainer";
 const MobileHeader = () => {
+  const [isMenu, setIsMenu] = useState(false);
   return (
-    // <div className='container-fluid bg-dark'>
+    <div className='mobHeader'>
+      <div className='row mt-4'>
+        <div className='col-md-12'>
+          <div className='mainMob'>
+            <div className='icon active'>
+              <MdHome />
+            </div>
+            <div className='icon'>
+              <MdSearch />
+            </div>
+            <div className='icon' onClick={() => setIsMenu(!isMenu)}>
+              <MdAddShoppingCart />
+            </div>
+            {isMenu && <CartContainer close={() => setIsMenu(!isMenu)} />}
 
-    <div className='row mt-4'>
-      <div className='col-md-12'>
-        <div className='mainMob'>
-          <div className='icon active'>
-            <MdHome />
-          </div>
-          <div className='icon'>
-            <MdSearch />
-          </div>
-          <div className='icon'>
-            <MdAddShoppingCart />
-          </div>
-          <div className='icon'>
-            <AiFillSetting />
+            <div className='icon'>
+              <AiFillSetting />
+            </div>
           </div>
         </div>
       </div>
     </div>
-    // </div>
   );
 };
 
