2d1707a15ef391dd0fa39c6de41ad293fb5a920f
wabbi-khan <kwahab789@gmail.com>
2022-10-18 16:45:14 +0500

    ok

--- src/components/Watches.jsx
+++ src/components/Watches.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 const Watches = () => {
   return (
     <div className='container mt-4'>
@@ -8,6 +7,12 @@ const Watches = () => {
           <div className='wacthHead'>
             <h1>everyone's desires</h1>
             <span>Watches</span>
+          </div>
+        </div>
+        <div className='col-md-12'>
+          <div className='watchBtnBg'>
+            <div className='watchIcnon'>icon</div>
+            <div className='watchName'>Rolex</div>
           </div>
         </div>
       </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,12 @@ import React from "react";
 import Hero from "../components/Hero";
 import Products from "../components/Products";
 import DebitCardSection from "../components/DebitCardSection";
-// import SpringWinter from "../components/SpringWinter";
 const Home = () => {
   return (
     <div>
       <Hero />
       <Products />
       <DebitCardSection />
-      {/* <SpringWinter /> */}
     </div>
   );
 };
