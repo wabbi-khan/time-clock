1a39263a72662349aef217b513727348eaf5dc78
wabbi-khan <kwahab789@gmail.com>
2022-10-19 15:28:35 +0500

    ok

--- src/components/Watches.jsx
+++ src/components/Watches.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { WatchData } from "../utils/data";
 import { motion } from "framer-motion";
+import ProductCard from "./ProductCard";
+import { productsData } from "../utils/products";
+// import axios from "axios";
 const Watches = () => {
+  const [items, setItems] = useState(productsData);
+  // console.log(productsData);
+  // const [data, setData] = useState([]);
+  // useEffect(() => {
+  //   const fetchingProducts = async () => {
+  //     try {
+  //       const res = await axios.get("http://localhost:5000/products");
+  //       console.log(res.data);
+  //       setData(res.data);
+  //     } catch (err) {
+  //       console.log(err);
+  //     }
+  //   };
+  //   fetchingProducts();
+  // }, []);
+  const FilterItems = (nameItem) => {
+    const updateItems = productsData.filter(
+      (currentName) => currentName.title === nameItem
+    );
+    setItems(updateItems);
+  };
   return (
     <div className='container mt-4'>
       <div className='row'>
@@ -19,11 +43,27 @@ const Watches = () => {
                 whileHover={{ scale: 1.2 }}
                 className='watchBtnBg'
                 key={item.id}
+                onClick={() => FilterItems(item.name)}
               >
                 <div className='watchIcnon'>{item.icon}</div>
                 <div className='watchName mt-2'> {item.name} </div>
               </motion.button>
             ))}
+          </div>
+        </div>
+        <div className='row mt-4'>
+          <div className='col-md-12'>
+            <div className=' d-flex scroll-x'>
+              {items.map((item) => (
+                <ProductCard
+                  key={item}
+                  title={item.title}
+                  price={item.price}
+                  img={item?.Image}
+                  item={item}
+                />
+              ))}
+            </div>
           </div>
         </div>
       </div>
