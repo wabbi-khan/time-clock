1d2546a9ad474fe59ee320352a78a6b3399aa752
wabbi-khan <kwahab789@gmail.com>
2022-10-20 20:28:23 +0500

    ok

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -37,7 +37,7 @@ const NavBar = () => {
             <span className='navbar-toggler-icon'></span>
           </button>
           <div className='collapse navbar-collapse' id='navbarNav'>
-            <ul className='navbar-nav mx-auto me-4 text-center'>
+            <ul className='navbar-nav mx-auto  text-center'>
               <li className='nav-item'>
                 <Link
                   className='nav-link active-link'
@@ -67,23 +67,25 @@ const NavBar = () => {
                   Contact
                 </Link>
               </li>{" "}
-              <li className='nav-item'>
+              <li className='nav-item d-sm-none'>
                 <div className='nav-circle'></div>
               </li>
             </ul>
             <ul className='navbar-nav me-4 text-center nav-icons'>
               <li className='nav-item'>
-                <Link className='nav-link ' to={"/"}>
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    width='20'
-                    height='20'
-                    fill='currentColor'
-                    class='bi bi-search'
-                    viewBox='0 0 16 16'
-                  >
-                    <path d='M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z' />
-                  </svg>
+                <Link className='nav-link' to={"/"}>
+                  <div className='searchIcon'>
+                    <svg
+                      xmlns='http://www.w3.org/2000/svg'
+                      width='20'
+                      height='20'
+                      fill='currentColor'
+                      class='bi bi-search'
+                      viewBox='0 0 16 16'
+                    >
+                      <path d='M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z' />
+                    </svg>
+                  </div>
                 </Link>
               </li>
               <li className='nav-item ms-4'>
